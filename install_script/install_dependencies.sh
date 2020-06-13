a27534519dee5aa9941e4f65f3965fc9068fa94e
Oswaldo Ferro <ojferro@gmail.com>
2020-06-12 20:15:44 -0700

    Move install scripts to their own directory

--- install_script/install_dependencies.sh
+++ install_script/install_dependencies.sh
@@ -2,9 +2,10 @@ echo "Updating apt-get"
 sudo apt-get update
 
 echo "Installing system packages"
-sudo apt-get install python3.8
 sudo apt-get -y install python3-pip
 sudo apt-get install python3-catkin-tools
+sudo apt install ros-melodic-joint-state-publisher-gui
+sudo apt install ros-melodic-rqt
 
 echo "Installing pip packages"
-pip3 install -r ./requirements.txt
+pip install -r ./requirements.txt
