07e1f925ba3c7b859766d032a189796e7fab7f9c
Oswaldo Ferro <ojferro@gmail.com>
2020-07-07 17:04:41 -0700

    Publishing base_link tf2

--- install_script/install_dependencies.sh
+++ install_script/install_dependencies.sh
@@ -1,4 +1,5 @@
 echo "Updating apt-get"
+sudo apt update
 sudo apt-get update
 
 echo "Installing system packages"
@@ -6,6 +7,9 @@ sudo apt-get -y install python3-pip
 sudo apt-get install python3-catkin-tools
 sudo apt install ros-melodic-joint-state-publisher-gui
 sudo apt install ros-melodic-rqt
+
+echo "Installing tf2 with python3 support"
+sudo apt install python3-numpy python3-catkin-pkg-modules python3-rospkg-modules python3-empy
 
 echo "Installing pip packages"
 pip install -r ./requirements.txt
