f073694cb168e3223691bd0b7def58c44d4ba0a5
Oswaldo Ferro <ojferro@gmail.com>
2020-07-08 15:10:46 -0700

    Cleanup dummy grid publisher

--- spot_ros_interface/scripts/dummy_occupancy_grid_publisher.py
+++ spot_ros_interface/scripts/dummy_occupancy_grid_publisher.py
@@ -16,14 +16,7 @@ occ_grid_pub = rospy.Publisher(
     "occupancy_grid", visualization_msgs.msg.MarkerArray, queue_size=20)
 
 rospy.init_node('dummy_occupancy_grid_pub')
-rate = rospy.Rate(1)  # Update at 10hz
-
-def convert_np_list_to_marker_array(grid, tf_to_trf_cell):
-    marker = visualization_msgs.msg.Marker()
-    marker_array = visualization_msgs.msg.MarkerArray()
-    
-    # tf_to_trf_cell is the transform (just position) to the top right front cell
-    # TODO: This
+rate = rospy.Rate(1)  # Update at 1 hz
 
 start_time = int(rospy.Time().now().secs)+5
 while not rospy.is_shutdown():
@@ -36,18 +29,23 @@ while not rospy.is_shutdown():
 
     marker.header.seq=0
     marker.header.stamp= rospy.Time()
+    marker.id = 0
     marker.header.frame_id= "base_link" #Must be map or another frame that exists (e.g. Spot's ko_frame)
     marker.type = visualization_msgs.msg.Marker.CUBE
     marker.action = visualization_msgs.msg.Marker.ADD
     marker.pose.position.x = 1 + int(rospy.Time().now().secs)-start_time
     marker.pose.position.y = 1
     marker.pose.position.z = 1
+    marker.pose.orientation.x = 0.0
+    marker.pose.orientation.y = 0.0
+    marker.pose.orientation.z = 0.0
+    marker.pose.orientation.w = 1.0
     marker.scale.x = 0.1
     marker.scale.y = 0.1
     marker.scale.z = 0.1
     marker.color.r = 1.0
-    marker.color.g = 0.5
-    marker.color.b = 0.5
+    marker.color.g = 0.0
+    marker.color.b = 0.0
     marker.color.a = 1.0
     
     grid.markers.append(marker)
