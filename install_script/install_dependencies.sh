d51d0e7b05f169335dc528884d99a08c83492ad3
Oswaldo Ferro <ojferro@gmail.com>
2020-07-29 20:34:59 -0700

    Update install script to reflect move to Noetic

--- install_script/install_dependencies.sh
+++ install_script/install_dependencies.sh
@@ -5,11 +5,9 @@ sudo apt-get update
 echo "Installing system packages"
 sudo apt-get -y install python3-pip
 sudo apt-get install python3-catkin-tools
-sudo apt install ros-melodic-joint-state-publisher-gui
-sudo apt install ros-melodic-rqt
 
-echo "Installing tf2 with python3 support"
-sudo apt install python3-numpy python3-catkin-pkg-modules python3-rospkg-modules python3-empy
+sudo apt-get install ros-noetic-rosbridge-server
+sudo apt-get install python-is-python3 #to point "python" to "python3"
 
 echo "Installing pip packages"
 pip install -r ./requirements.txt
